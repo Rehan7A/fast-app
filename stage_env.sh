8f8a11057774f8f51fd4fbac111652ca26ba4b19
Rehan Ansari <rehanansari6565@gmail.com>
2024-08-24 20:31:15 +0530

    referencing correct env variable

--- stage_env.sh
+++ stage_env.sh
@@ -1,4 +1,4 @@
-echo "API_URL=${API_URL}" > .env
-echo "GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}" >> .env
+echo "API_URL=${vars.API_URL}" > .env
+echo "GOOGLE_MAPS_API_KEY=${vars.GOOGLE_MAPS_API_KEY}" >> .env
 
 echo ".env file created successfully!"
